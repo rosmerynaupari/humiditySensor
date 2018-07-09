7564ba418d84e703897d6e4435634f88380b8251
humiditySensor <rnauparia@uni.pe>
2018-07-08 19:23:22 -0500

    Add files via upload

--- humuiditySensor.ino
+++ humuiditySensor.ino
@@ -1,4 +1,8 @@
+#include <Time.h>
+
 #include <Adafruit_Sensor.h>
+
+#include <Time.h>//Incluimos la libreria Time
 
 #include <DHT_U.h>
 #include <DHT.h>
@@ -16,6 +20,10 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+  
+  //Formato: hora, minutos, segundos,dias, mes, año
+  setTime(9,35,00,8,07,2018);
+  
   Serial.println("Humedad (%) y Temperatura (*C)");
 
   dht.begin();
@@ -24,7 +32,24 @@ void setup() {
 void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
-
+  
+  time_t p = now();  //variable time_t p
+  
+  //Imprimimos fecha y hora
+  
+  Serial.print(day(p));
+  Serial.print(+ "/");
+  Serial.print(month(p));
+  Serial.print(+ "/");
+  Serial.print(year(p));
+  Serial.print(" ");
+  Serial.print(hour(p));
+  Serial.print(+ ":");
+  Serial.print(minute(p));
+  Serial.print(+":");
+  Serial.print(second(p));
+  Serial.print(" ");
+  
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
@@ -32,7 +57,7 @@ void loop() {
   
   Serial.print("Humidity: ");
   Serial.print(h);
-  Serial.print(" %\t");
+  Serial.print(" ");
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.println(" *C ");
